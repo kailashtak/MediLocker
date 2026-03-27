755ca47347adee40238ef8d82521fede4bdef6ef
Kailash Tak <kailash.tak0303@gmail.com>
2026-03-27 19:20:27 +0530

    on upload branch wokring somewhat

--- config/wagmi.js
+++ config/wagmi.js
@@ -1,11 +1,23 @@
+// import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+// import { /*localhost,*/ sepolia } from "wagmi/chains";
+
+// const PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
+
+// export const config = getDefaultConfig({
+//   appName: "Health DApp",
+//   projectId: PROJECT_ID,
+//   chains: [sepolia],
+//   ssr: true,
+// });
+
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { /*localhost,*/ sepolia } from "wagmi/chains";
+import { localhost, sepolia } from "wagmi/chains";
 
 const PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
 
 export const config = getDefaultConfig({
   appName: "Health DApp",
   projectId: PROJECT_ID,
-  chains: [sepolia],
+  chains: [localhost], // ✅ ONLY localhost
   ssr: true,
 });
